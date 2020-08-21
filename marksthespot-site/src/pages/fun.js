1eda068fc38070f3ad3654c8fb38b1d7d864ab16
Mark <marklim4@gmail.com>
2020-08-21 12:13:34 -0400

    all the shit

---
+++ marksthespot-site/src/pages/fun.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const FunPage = () => (
+  <Layout>
+    <SEO title={"Miscellaneous"}/>
+    <div className = "banner">
+      <h1 style = {{textAlign: "center"}}>Miscellaneous</h1>
+    </div>
+  </Layout>
+)
+
+export default FunPage
